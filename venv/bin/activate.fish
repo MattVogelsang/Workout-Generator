92b611f87e77699f991d5872e8f2415bc261a6ee
Mattvogelsang <vogelsangmatt@gmail.com>
2025-04-21 19:30:12 -0400

    added dark mode

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/mattvogelsang/Desktop/Code Projects/Workout-Generator/venv"
+set -gx VIRTUAL_ENV "/Users/mattvogelsang/Desktop/Code Projects/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
